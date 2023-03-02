741226e0767b924d8b8980a0763f960019838ff1
André Cosolin <75508214+andrecosolin@users.noreply.github.com>
2023-03-01 21:12:14 -0300

    Exercicios Resolvidos BeeCrowd

---
+++ 1014.py
@@ -0,0 +1,3 @@
+km = int(input('Digite o km percorrido: '))
+G = float(input('Digite o gasto de combustível: '))
+print('{:.1f} km/l'.format(km/G))
---
+++ 1017.py
@@ -0,0 +1,4 @@
+tempo = int(input())
+vel = int(input())
+qtde = (tempo * vel) / 12
+print('{:.3f}'.format(qtde))
